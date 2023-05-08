680c38c8a6bd850732825720626f80eda4a23efb
Taiwo Jolomi <taiwojolomi15@gmail.com>
2023-05-08 01:56:17 +0100

    Working on Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,36 @@
 import React, { useEffect } from 'react'
-import { AiOutlineMenu, AiOutlineMenuFold } from 'react-icons/ai'
+import { AiOutlineMenu } from 'react-icons/ai'
 import { FiShoppingCart } from 'react-icons/fi'
 import { BsChatLeft } from 'react-icons/bs'
 import { RiNotification3Line } from 'react-icons/ri'
 import { MdKeyboardArrowDown } from 'react-icons/md'
 import { TooltipComponent } from '@syncfusion/ej2-react-popups'
-import avatar from '../data/dummy'
+
+import avatar from '../data/avatar.jpg'
 import { Cart, Chat, Notification, UserProfile } from '.'
 import { useStateContext } from '../contexts/ContextProvider'
 
-const NavButton = ({title, customFunc, icon, color, dotColor}) => {
+const NavButton = ({ title, customFunc, icon, color, dotColor }) => {
     <TooltipComponent content={title} position="BottomCenter">
-        <button type='button' title={title} onClick={customFunc} style={{color}}></button>
+        <button
+            type='button'
+            title={title}
+            onClick={customFunc}
+            style={{ color }}
+            className="relative text-xl rounded-full p-3 hover:bg-light-gray"
+        >
+            <span style={{ background: dotColor }}
+                className='absolute inline-flex rounded-full h-2 right-2 w-2 top-10'
+            />{icon}
+        </button>
     </TooltipComponent>
 }
 
 const Navbar = () => {
-    const {ativeMenu, setActiveMenu} = useStateContext()
+    const { activeMenu, setActiveMenu } = useStateContext()
     return (
-        <div className='flex justify-between p-2 md:mx-6 relative'>
-            <NavButton title='Menu' customFunc={()=> setActiveMenu((prevMenuState) => !prevMenuState)} color="blue" icon={<AiOutlineMenu />} />
+        <div className="flex justify-between p-2 md:ml-6 md:mr-6 relative">
+            <NavButton title='Menu' customFunc={() => setActiveMenu((prevMenuState) => !prevMenuState)} color="blue" icon={<AiOutlineMenu />} />
         </div>
     )
 }
