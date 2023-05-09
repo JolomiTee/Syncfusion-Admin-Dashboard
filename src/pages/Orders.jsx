1238259cbee869a3cfbaba61f4056b882399bef6
Taiwo Jolomi <taiwojolomi15@gmail.com>
2023-05-09 11:19:39 +0100

    building grid table component

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { GridComponent, ColumnDirective, ColumnsDirective, Resize, Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit, Inject } from '@syncfusion/ej2-react-grids'
+import {ordersData, contextMenuItems, ordersGrid} from '../data/dummy';
+import {Header} from '../components'
+
 
 const Orders = () => {
-  return (
-    <div>Orders</div>
-  )
+    return (
+        <div className="mt-12 md:mt-0 p-2 md:p-10 bg-white rounded-3xl">
+            <Header category="Page" title="Orders" />
+            <GridComponent
+                id='grid-component'
+                dataSource={ordersData}
+            >
+                <ColumnsDirective>
+                    {ordersGrid.map((item, index) => (
+                        <ColumnDirective key={index} {...item} />
+                    ))}
+                </ColumnsDirective>
+            </GridComponent>
+        </div>
+    )
 }
 
 export default Orders
