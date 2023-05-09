aedf793aeb592149466efd89bdf7b4347597bedb
Taiwo Jolomi <taiwojolomi15@gmail.com>
2023-05-09 22:23:14 +0100

    Created Customers Page with table data. Features: * Update and * Delete of some fields * Pagination

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
-
+import { GridComponent, ColumnDirective, ColumnsDirective, Page, Selection, Inject, Edit, Toolbar, Sort, Filter } from '@syncfusion/ej2-react-grids'
+import { customersData, customersGrid } from '../data/dummy'
+import { Header } from '../components'
 const Customer = () => {
-  return (
-    <div>Customer</div>
-  )
+    return (
+        <div className="mt-12 md:mt-0 p-2 md:p-10 bg-white rounded-3xl">
+            <Header category="Page" title="Customers" />
+            <GridComponent
+                dataSource={customersData}
+                allowSorting
+                allowPaging
+                toolbar={['Delete']}
+                width="auto"
+                editSettings={{
+                    allowDeleting: true,
+                    allowEditing: true
+                }}
+            >
+                <ColumnsDirective>
+                    {customersGrid.map((item, index) => (
+                        <ColumnDirective key={index} {...item} />
+                    ))}
+                </ColumnsDirective>
+                <Inject services={[Page, Toolbar, Selection, Edit, Sort, Filter]} />
+            </GridComponent>
+        </div>
+    )
 }
 
 export default Customer
