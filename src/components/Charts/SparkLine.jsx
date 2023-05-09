ba8f9e24dbc3e994db2c9374d71f294b76a44035
Taiwo Jolomi <taiwojolomi15@gmail.com>
2023-05-09 10:06:18 +0100

    Initial Layout of Ecommerce page complete. Now moving on to Orders.jsx page

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,31 +1,31 @@
 import React from 'react'
-import {SparklineComponent, Inject, SparklineTooltip} from '@syncfusion/ej2-react-charts'
+import { SparklineComponent, Inject, SparklineTooltip } from '@syncfusion/ej2-react-charts'
 
-const SparkLine = ({id, height, width, color, data, type, currentColor}) => {
-  return (
-    <SparklineComponent
-        id={id}
-        height={height}
-        width={width}
-        lineWidth={1}
-        valueType="Numeric"
-        fill={color}
-        border={{color: currentColor, width: 2}}
-        dataSource={data}
-        xName='x'
-        yName='y'
-        type={type}
-        tooltipSettings={{
-            visible: true,
-            format: '${x} : data ${y}',
-            trackLineSettings: {
-                visible: true
-            }
-        }}
-    >
-        <Inject services={[SparklineTooltip]} />
-    </SparklineComponent>
-  )
+const SparkLine = ({ id, height, width, color, data, type, currentColor }) => {
+    return (
+        <SparklineComponent
+            id={id}
+            height={height}
+            width={width}
+            lineWidth={1}
+            valueType="Numeric"
+            fill={color}
+            border={{ color: currentColor, width: 2 }}
+            dataSource={data}
+            xName='x'
+            yName='yval'
+            type={type}
+            tooltipSettings={{
+                visible: true,
+                format: 'deficit: ${x}% : increase: ${yval}%',
+                trackLineSettings: {
+                    visible: true
+                }
+            }}
+        >
+            <Inject services={[SparklineTooltip]} />
+        </SparklineComponent>
+    )
 }
 
 export default SparkLine
