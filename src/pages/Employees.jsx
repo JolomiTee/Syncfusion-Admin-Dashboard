84fafa61ddaf0bf99f322c2432eddd721d0d6d48
Taiwo Jolomi <taiwojolomi15@gmail.com>
2023-05-09 21:57:52 +0100

    Created Employees page and component which is similar to the Orders component

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
-
+import { GridComponent, ColumnDirective, ColumnsDirective, Page, Search, Inject, Toolbar } from '@syncfusion/ej2-react-grids'
+import { employeesData, contextMenuItems, employeesGrid } from '../data/dummy';
+import { Header } from '../components'
 const Employee = () => {
-  return (
-    <div>Employee</div>
-  )
+    return (
+        <div className="mt-12 md:mt-0 p-2 md:p-10 bg-white rounded-3xl">
+            <Header category="Page" title="Employees" />
+            <GridComponent
+                dataSource={employeesData}
+                allowSorting
+                allowPaging
+                toolbar={['Search']}
+                width="auto"
+            >
+                <ColumnsDirective>
+                    {employeesGrid.map((item, index) => (
+                        <ColumnDirective key={index} {...item} />
+                    ))}
+                </ColumnsDirective>
+                <Inject services={[ Page, Search, Toolbar]} />
+            </GridComponent>
+        </div>
+    )
 }
 
 export default Employee
