173d9890fbcc59a1291ff253e5c6c52e4715bd00
Taiwo Jolomi <taiwojolomi15@gmail.com>
2023-05-10 21:44:29 +0100

    Created Color picker component. All Apps are complete. Working on Navbar items functionality now

--- src/pages/ColorPicker.jsx
+++ src/pages/ColorPicker.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
+import { ColorPickerComponent } from '@syncfusion/ej2-react-inputs'
+import { Header } from '../components'
+const changeColor = (args) => {
+    document.getElementById('preview').style.backgroundColor = args.currentValue.hex
+}
 
 const ColorPicker = () => {
-  return (
-    <div>ColorPicker</div>
-  )
+
+    return (
+        <div className='m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl'>
+            <Header category="App" title="Color Picker" />
+
+            <div className="text-center">
+                <div id='preview'/>
+                <div className="flex justify-center items-ce gap-20 flex-wrap">
+                    <div className="">
+                        <p className='text-2x; font-semibold mt-2 mb-4'>Inline Palette</p>
+                        <ColorPickerComponent
+                            id='inline-pallete'
+                            mode='Palette'
+                            modeSwitcher={false}
+                            inline
+                            showButtons={false}
+                            change={changeColor}
+                        />
+                    </div>
+                    <div className="">
+                        <p className='text-2x; font-semibold mt-2 mb-4'>Inline Picker</p>
+                        <ColorPickerComponent
+                            id='inline-pallete'
+                            mode='Picker'
+                            modeSwitcher={false}
+                            inline
+                            showButtons={false}
+                            change={changeColor}
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default ColorPicker
