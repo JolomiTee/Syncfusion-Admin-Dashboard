c426cc0efef3f8ded489357a7edf67185d9773f4
Taiwo Jolomi <taiwojolomi15@gmail.com>
2023-05-11 13:12:45 +0100

    Registered App with the Syncfusion License key

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { ContextProvider } from './contexts/ContextProvider.jsx';
+import { registerLicense } from '@syncfusion/ej2-base'
+registerLicense(import.meta.env.VITE_SYNC_FUSION_LICENCE_KEY);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ContextProvider>
